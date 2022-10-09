f3d357e33d82d91f789bace146fb3f6d1fe8d2a6
Artur <artur.souza955@gmail.com>
2022-10-09 20:14:19 -0300

    criei uma calculadora simples

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'estudos';
+  tela = "";
+  simbolos : string[] = ['1', '2', '3','4','5','6','7','8','9','0','+','-','*','/'];
+  digitou(letra:string): void{
+    this.tela += letra; 
+  }
+  result(): void{
+    this.tela = eval(this.tela)
+  }
+  limptela(): void{
+    this.tela = "";
+  }
+}
